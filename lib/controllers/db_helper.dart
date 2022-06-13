a24c7d2a4adcf30ac80ece1f162c6f6e6d7f4815
ishaque <ishaque.kannoth@gmail.com>
2022-06-13 19:56:00 +0530

    untilDBstart

---
+++ lib/controllers/db_helper.dart
@@ -0,0 +1,32 @@
+import 'package:hive/hive.dart';
+
+class Dbhelper {
+  late Box box;
+
+  Dbhelper() {
+    openBox();
+  }
+
+  openBox() {
+    box = Hive.box('money');
+  }
+
+  Future addData(int amount, DateTime date, String note, String type) async {
+    var value = {
+      'amount': amount,
+      'date': date,
+      'note': note,
+      'type': type,
+    };
+    box.add(value);
+  }
+
+  Future<Map> fetchData() {
+    if (box.values.isEmpty) {
+      return Future.value({});
+    } else {
+      return Future.value(box.toMap());
+    }
+   
+  }
+}
